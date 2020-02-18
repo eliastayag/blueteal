807a1f1d3937789f79a372b526f6a14bc73784f1
eliastayag <misterghost999@gmail.com>
2020-02-18 01:03:39 -0800

    book

--- src/pages/comps/ClassCard/ClassCard.js
+++ src/pages/comps/ClassCard/ClassCard.js
@@ -3,9 +3,12 @@ import './ClassCard.scss'
 
 
 
+
 function ClassCard({itemTitle,onClick,active,style}){
     return(
         <div className='classcard-1' id={style} onClick={onClick} >
+            <img src={require('../../../imgs/icons/png/book.black.png')}/>
+
             {itemTitle}
         </div>
 
