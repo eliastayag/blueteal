4a7091c20dc0851db5c2e9b7560708fe4c581516
Doris Ho <doris.h@live.ca>
2020-03-13 23:25:04 -0700

    Added purchase confirmation screen

---
+++ src/stories/13-ConfirmationPremium.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ConfirmationPremium from '../pages/Confirmation/ConfirmationPremium';
+
+export default {
+    title: 'ConfirmationPremium',
+    component: ConfirmationPremium,
+};
+
+export const ConfirmationPremiumTab =()=>{
+    return <ConfirmationPremium/>;
+}
