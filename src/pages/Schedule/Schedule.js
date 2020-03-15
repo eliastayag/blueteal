2d817f58727f6c868af096bf3ffcf6e217eb578f
josephbondoc <josephsantosbondoc@gmail.com>
2020-03-15 12:47:50 -0700

    WIP: schedule color change, need to finish grid

--- src/pages/Schedule/Schedule.js
+++ src/pages/Schedule/Schedule.js
@@ -10,17 +10,19 @@ function Schedule() {
     <div className="sched-comp">
       <div style={{display:"flex", flexDirection:"column"}}>
         <p>8:00</p>
-        <p>8:00</p>
-        <p>8:00</p>
-        <p>8:00</p>
-        <p>8:00</p>
+        <p>9:00</p>
+        <p>10:00</p>
+        <p>11:00</p>
+        <p>12:00</p>
       </div>
       <div className="sched-days">
-        <p className="sched-days-box sd1">Mon</p>
-        <p className="sched-days-box sd2">Tues</p>
-        <p className="sched-days-box sd3">Wed</p>
-        <p className="sched-days-box sd4">Thu</p>
-        <p className="sched-days-box sd5">Fri</p>
+        <p className="sched-days-box sd1"></p>
+        <p className="sched-days-box sd2">Mon</p>
+        <p className="sched-days-box sd3">Tues</p>
+        <p className="sched-days-box sd4">Wed</p>
+        <p className="sched-days-box sd5">Thu</p>
+        <p className="sched-days-box sd6">Fri</p>
+
       </div>
       <div style={{display:"flex", flexDirection:"row"}}>
         <div className="sched-box1"></div>
