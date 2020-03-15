98009dcccd9d409fa206bd296f62c1fe207334d4
eliastayag <misterghost999@gmail.com>
2020-03-15 01:40:19 -0700

    schedule started someone finish

--- src/pages/Schedule/Schedule.js
+++ src/pages/Schedule/Schedule.js
@@ -1,20 +1,35 @@
 import React, {useState} from 'react';
 
-import Menu from '../comps/Menu/Menu';
-import Header from '../comps/Header/Header';
-import theme from '../../styles/ThemeStyles';
-import Button from '../comps/Button/Button';
+import './Schedule.scss'
 
 
 
 function Schedule() {
     
   return (
-    <div >
-
-
-
-    
+    <div className="sched-comp">
+      <div style={{display:"flex", flexDirection:"column"}}>
+        <p>8:00</p>
+        <p>8:00</p>
+        <p>8:00</p>
+        <p>8:00</p>
+        <p>8:00</p>
+      </div>
+      <div className="sched-days">
+        <p className="sched-days-box sd1">Mon</p>
+        <p className="sched-days-box sd2">Tues</p>
+        <p className="sched-days-box sd3">Wed</p>
+        <p className="sched-days-box sd4">Thu</p>
+        <p className="sched-days-box sd5">Fri</p>
+      </div>
+      <div style={{display:"flex", flexDirection:"row"}}>
+        <div className="sched-box1"></div>
+        <div className="sched-box2"></div>
+        <div className="sched-box3"></div>
+        <div className="sched-box4"></div>
+        <div className="sched-box5"></div>
+        <div className="sched-box6"></div>
+      </div>    
     </div>
 
   );
